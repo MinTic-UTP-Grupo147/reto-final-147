ba68d710a0e7b34ac26c5f583da94043fe5ea36f
sebasjare08 <sebasjara@gmail.com>
2020-12-17 10:52:08 -0500

    reto4

--- routes/api/categoria.js
+++ routes/api/categoria.js
@@ -3,13 +3,14 @@ const router = require('express').Router();
 
 // si tuvieramos un controlador seria
 const categoriaController = require('../../controllers/CategoriaController');
+const { verificarAdministrador } = require('../../middlewares/auth');
 // importamos el middleware de autenticacion
 const auth = require('../../middlewares/auth')
 
-router.get('/list', categoriaController.list);
-router.post('/add', categoriaController.add);
-router.put('/update', categoriaController.update);
-router.put('/activate', categoriaController.activate);
-router.put('/deactivate', categoriaController.deactivate);
+router.get('/list',auth.verificarAdministrador, categoriaController.list);
+router.post('/add',auth.verificarAdministrador, categoriaController.add);
+router.put('/update',auth.verificarAdministrador, categoriaController.update);
+router.put('/activate',auth.verificarAdministrador, categoriaController.activate);
+router.put('/deactivate',auth.verificarAdministrador, categoriaController.deactivate);
 
 module.exports = router;
