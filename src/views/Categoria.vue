b187825265d0edc4269316ee6cb8384288604ad5
sebasjare08 <sebasjara@gmail.com>
2020-12-17 19:36:16 -0500

    primer commit

---
+++ src/views/Categoria.vue
@@ -0,0 +1,13 @@
+<template>
+    <la-categoria></la-categoria>
+</template>
+
+<script>
+import LaCategoria from '../components/LaCategoria'
+ 
+export default {
+    components:{
+        LaCategoria,
+    }
+}
+</script>
