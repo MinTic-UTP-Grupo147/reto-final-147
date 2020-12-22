d0316a9869bd571cc3ee0fd907ee647da92ddbed
sebasjare08 <sebasjara@gmail.com>
2020-12-22 13:56:48 -0500

    login cambio de posicion

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,55 +1,49 @@
 <template>
-    <footer class="page-footer bg-secondary text-white pt-4">
-        
-        
-            <!-- Footer Text -->
-            <div class="container-fluid text-center text-md-left">
-                <!-- Grid row -->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-6 mt-md-0 mt-3">
-                        <!-- Content -->
-                        <h5 class="text-uppercase font-weight-bold">Acerca de:</h5>
-                        <p>Éste proyecto nace de la necesidad de la industria del deporte de mejorar su rendimiento 
-                            general, basados en las estadisticas de jugadores y equipos, para el crecimiento en ventas e indicadores 
-                            financieros, para asi impulsar el desarrollo de las comunidades donde jugamos y vivimos.<br><br> 
-                            <b>¿Quisieras contribuir en ésta causa?</b> <a href="https://github.com/MinTic-UTP-Grupo147/semana-2-147.git" class="btn btn-primary">Enlace GitHub</a> 
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-                    <hr class="clearfix w-100 d-md-none pb-3">
-                    <!-- Grid column -->
-                    <div class="col-md-6 mb-md-0 mb-3">
-                        <!-- Content -->
-                        <h5 class="text-uppercase font-weight-bold">Nuestro equipo:</h5>
-                        <div>
-                            <h6>Geraldine Hernández - DEVELOPER - lghv21@gmail.com</h6>          
+     <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="indigo lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+            href="https://github.com/MinTic-UTP-Grupo147/reto-final-147"
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+  
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
 
-                            <h6>José Chavarrio - DEVELOPER - jchavarr78@gmail.com</h6>     
-                                
-                            <h6>Harold Avendaño - DEVELOPER - harolavendanoz18@gmail.com</h6>    
-                                    
-                            <h6>Sebastian Jaramillo - DEVELOPER - sebasjara@gmail.com</h6>    
-                
-                            <h6>Juan Camilo Patiño - DEVELOPER - jcpat78@gmail.com</h6>
-                            
-                        </div>
-                    </div>
-                    <!-- Grid column -->
-                </div>
-                <!-- Grid row -->
-            </div>
-            <!-- Footer Text -->
-            <!-- Copyright -->
-            <div class="footer-copyright text-center py-1 bg-dark">(c) 2020 Copyright: Grupo 147
-            </div>
-            <!-- Copyright -->
-        
-    </footer>
 </template>
 
 <script>
 export default {
   name: "PageFooter",
+  data: () => ({
+    icons: [
+      'mdi-github',
+      
+    ],
+  }),
 };
 </script>
--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -1,98 +1,82 @@
 <template>
-     <header>
-       <v-app-bar class="d-sm-none">
-      <v-app-bar-nav-icon 
-      @click="drawer = !drawer;"></v-app-bar-nav-icon>
-         
+  <header>
+    <v-app-bar class="d-sm-none">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" fixed temporary>
       <v-card class="mx-auto" width="300">
         <v-list>
-          <v-list-item :to ="{name:'Home'}">
+          <v-list-item :to="{ name: 'Home' }">
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
 
             <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>  
-           
+          </v-list-item>
 
-              <v-list-item 
-              v-for="([title, icon,], i) in admins" :key="i" 
-              >
-                <v-list-item-title v-text="title"></v-list-item-title>
+          <v-list-item v-for="([title, icon], i) in admins" :key="i">
+            <v-list-item-title v-text="title"></v-list-item-title>
 
-                <v-list-item-icon>
-                  <v-icon v-text="icon"></v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-                    
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
         </v-list>
       </v-card>
     </v-navigation-drawer>
-
-        <v-tabs class="d-none d-sm-flex">
-            <v-tab href="#">Inicio</v-tab>
-            <v-tab href="#somos">Quiénes somos?</v-tab>
-            <v-tab href="#services"
-            >Servicios</v-tab>
-            <v-tab href="#casos">Casos de éxito</v-tab>
-        <v-tab> 
-      <v-btn
-      icon
-      class="mr-5"
-      href="login">
-        <v-icon>mdi-login</v-icon>
-        <span>Login</span>
-      </v-btn>
-        </v-tab>
-        </v-tabs>
-       
-            <!-- Imagen  carousel -->
-            <v-carousel>
-            <v-carousel-item
-                v-for="(item,i) in items"
-                :key="i"
-                :src="item.src"
-                reverse-transition="fade-transition"
-                transition="fade-transition"
-            ></v-carousel-item>
-            </v-carousel>
-           
-        </header>    
+<v-container class="grey lighten-5 d-flex justify-space-around">
+    <v-tabs class="d-none d-sm-flex justify-space-around">
+      <v-tab href="#">Inicio</v-tab>
+      <v-tab href="#somos">Quiénes somos?</v-tab>
+      <v-tab href="#services">Servicios</v-tab>
+      <v-tab href="#casos">Casos de éxito</v-tab>
+          
+    </v-tabs>
+    <v-btn icon cols="12" class="mr-5" href="login">
+          <v-icon>mdi-login</v-icon>
+          <span>Login</span>
+        </v-btn>
+</v-container>
+    <!-- Imagen  carousel -->
+    <v-carousel  height="300px">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        max-height="280px"
+      ></v-carousel-item>
+    </v-carousel>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'SectionHeader',
-    data: () => ({
-      drawer: null,
+  name: "SectionHeader",
+  data: () => ({
+    drawer: null,
     admins: [
-      ['Categoría', 'mdi-information', 'Categoria'],
-      ['Articulo', 'mdi-form-select', 'Articulo'],
+      ["Categoría", "mdi-information", "Categoria"],
+      ["Articulo", "mdi-form-select", "Articulo"],
     ],
-    cruds: [
-      ['Usuario', 'mdi-account-circle','Usuario'],
-      
-    ],
-    
-      items: [
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-        },
-      ],
-    
-}),
+    cruds: [["Usuario", "mdi-account-circle", "Usuario"]],
 
-} 
+    items: [
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+      },
+    ],
+  }),
+};
 </script>
